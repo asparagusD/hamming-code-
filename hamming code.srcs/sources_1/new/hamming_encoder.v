c07b5a5dfba56ad3cfaff8729e5659cb3a1be4a0
asparagusD <nufel1130@gmail.com>
2026-09-05 15:58:23 +0600

    initial commit

---
+++ hamming code.srcs/sources_1/new/hamming_encoder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/05/2026 03:56:30 PM
+// Design Name: 
+// Module Name: hamming_encoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hamming_encoder(
+
+    );
+endmodule
