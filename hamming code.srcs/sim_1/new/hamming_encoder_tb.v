9fa0e90ebb7af1bd0fbe31317cae4d842db539f9
asparagusD <nufel1130@gmail.com>
2026-09-05 17:13:03 +0600

    verify the design and finish off

--- hamming code.srcs/sim_1/new/hamming_encoder_tb.v
+++ hamming code.srcs/sim_1/new/hamming_encoder_tb.v
@@ -27,13 +27,27 @@ module hamming_encoder_tb(
     reg [3:0] data_in;
     wire [6:0] data_out;
     
-    hamming_encoder dut(data_in, data_out);
+    wire [3:0] data_out_decoder;
+    wire [2:0] error_pos;
+    wire error_detected;
+    wire [6:0] corrected_code;
+    wire [6:0] data_out_temp;
+    
+    assign data_out_temp = data_out + 1'b1;
+    
+    hamming_encoder dut1(data_in, data_out);
+    hamming_decoder dut2(data_out_temp, data_out_decoder, error_pos, error_detected, corrected_code);
     
     initial
         begin
             data_in = 4'b1101;
             #1;
             $display("the value of data_out is %b", data_out);
+            
+            #10;
+            $display("data received by decoder is %b", data_out_temp);
+            $display("the value of error_pos is %b", error_pos);
+            $display("corrected code from the decoder is %b", corrected_code);
         end
     
 endmodule
