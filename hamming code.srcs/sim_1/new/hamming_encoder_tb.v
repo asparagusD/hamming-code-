b3b3a0707597e4c8c6b74d224b983c4c9a42fb40
asparagusD <nufel1130@gmail.com>
2026-09-05 16:19:28 +0600

    start writing testbench for hamming encoder

---
+++ hamming code.srcs/sim_1/new/hamming_encoder_tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/05/2026 04:18:35 PM
+// Design Name: 
+// Module Name: hamming_encoder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hamming_encoder_tb(
+
+    );
+    
+    
+    
+endmodule
