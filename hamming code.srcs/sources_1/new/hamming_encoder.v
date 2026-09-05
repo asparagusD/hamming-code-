83369d739ab7f6a8860c1e6c996fd0344d7ef1e0
asparagusD <nufel1130@gmail.com>
2026-09-05 16:10:26 +0600

    declare and assign variables

--- hamming code.srcs/sources_1/new/hamming_encoder.v
+++ hamming code.srcs/sources_1/new/hamming_encoder.v
@@ -24,4 +24,14 @@ module hamming_encoder(
     input [3:0] data_in, 
     output [6:0] data_out
     );
+    
+    wire p1, p2, p3;
+    wire d1, d2, d3, d4;
+    
+    assign d1 = data_in[0];
+    assign d2 = data_in[1];
+    assign d3 = data_in[2];
+    assign d4 = data_in[3];
+    
+    
 endmodule
